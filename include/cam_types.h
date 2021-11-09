784e9ba5bac71482717d91bb8d65cdee69a004fe
anonymous <anonymous>
2021-11-09 03:07:37 +0000

    offset fix 1

--- include/cam_types.h
+++ include/cam_types.h
@@ -1248,6 +1248,7 @@ typedef struct {
     cam_dimension_t fd_frame_dim;              /* frame dims on which fd is applied */
     uint8_t update_flag;                       /* flag to inform whether HAL needs to send cb
                                                 * to app or not */
+    char test1[160];
 } cam_face_detection_data_t;
 
 // definition of composite face detection data
